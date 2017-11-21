3ab7b5a9a0d828834ad0f9bb85826bfdabe46a58
mahatehotia <mahatehotia@hotmail.com>
2017-11-21 20:51:52 +0100

    ajout du header

---
+++ html/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+<body>
+<div class="background">
+    <nav class="navbar navbar-light navBg">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">AMS</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i></span> Connexion</a></li>
+                <li><a href="#"><i class="fa fa-user-circle" aria-hidden="true"></i></span> Inscription</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="espaceVide"></div>
+    <nav class="navbar navbar-light navBg">
+        <div class="container-fluid">
+            <ul class="nav navbar-nav navbar-left">
+                <i class="fa fa-facebook-official"></i>
+                <i class="fa fa-instagram"></i>
+                <i class="fa fa-snapchat"></i>
+                <i class="fa fa-pinterest-p"></i>
+                <i class="fa fa-twitter"></i>
+                <i class="fa fa-linkedin"></i>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Accueil</a></li>
+                <li><a href="#"><i class="fa fa-shopping-bag" aria-hidden="true"></i> Boutique</a></li>
+                <li><a href="#"><i class="fa fa-book" aria-hidden="true"></i> Catalogue</a></li>
+                <li><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i> Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+</body>
+</html>
