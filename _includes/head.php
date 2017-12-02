f049ec2aa2b1f8a482f138abf5597b9751a2d07b
CHASSAGNE Aurelie <aurelie.chassagne@businessdecision.com>
2017-12-02 21:04:59 +0100

    Ajout envoi de mail mais serveur mail ne fonctionne pas

--- _includes/head.php
+++ _includes/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <!-- Icon du site -->
-    <title>AMultiService</title>
+    <title>AMS Multiservices</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
