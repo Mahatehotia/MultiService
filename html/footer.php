102b23fa2b217c40df4d999ce3e91e23cf21b73e
CHASSAGNE Aurelie <aurelie.chassagne@businessdecision.com>
2017-12-04 10:18:21 +0100

    ajout footer

--- html/footer.php
+++ html/footer.php
@@ -24,10 +24,18 @@
                 <div class="col-sm-5">
                 </div>
             </div>
+			<div class="row">
+                <div class="col-sm-5">
+                </div>
+                <div class="col-sm-2">
+                    88 Rue Des Tilleuls MARCILLEUX 01150 SAINT VULBAS
+                </div>
+                <div class="col-sm-5">
+                </div>
+            </div>
             </div>
         </div>
     </div>
-	88 Rue Des Tilleuls MARCILLEUX 01150 SAINT VULBAS
 </footer>
 
 </body>
