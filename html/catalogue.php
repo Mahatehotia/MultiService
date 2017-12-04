4de6924161e269727ec6d3488a73ae91e0c6e013
mahatehotia <mahatehotia@hotmail.com>
2017-12-04 09:09:38 +0100

    modif page catalogue partie user

--- html/catalogue.php
+++ html/catalogue.php
@@ -2,98 +2,67 @@
 require('../_includes/verification.php');
 include 'header.php';
 ?>
+<?php
+require '../classes/function.php';
+
+try
+{
+    // On se connecte à MySQL
+    $bdd = new PDO('mysql:host=localhost;dbname=ams;charset=utf8', 'tafacesi', 'tafacesi');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : '.$e->getMessage());
+}
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table catalogue
+$reponse = $bdd->query('SELECT * FROM catalogue');
+?>
 <div class="container">
-<div class="row">
+    <div class="row">
+        <div class="col-lg-4 col-sm-4"></div>
 
-    <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                    <a href="#">Item One</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-            </div>
+        <div class="col-lg-4 col-sm-4">
+            <h1>Notre catalogue</h1>
         </div>
+        <div class="col-lg-4 col-sm-4"></div>
     </div>
-
-    <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                    <a href="#">Item Two</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                    <a href="#">Item Three</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                    <a href="#">Item Four</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                    <a href="#">Item Five</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/200x200" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                    <a href="#">Item Six</a>
-                </h4>
-                <h5>$24.99</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-            </div>
-        </div>
-    </div>
-
 </div>
-<!-- /.row -->
+<div class="container">
+    <div class="row">
+        <?php
 
-</div>
-<!-- /.col-lg-9 -->
+        // On affiche chaque entrée une à une
+        $donnees = $reponse->fetchAll();
+        for ($i=0; $i<count($donnees);$i++){
+            ?>
+            <div class="col-lg-4 col-md-4 mb-4">
+                <div class="card h-100">
+                    <img class="card-img-top" src="<?php echo $donnees[$i]['imageService']; ?>" alt="<?php echo $donnees[$i]['imageService']; ?>">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <?php echo $donnees[$i]['nomService']; ?>
+                        </h4>
+                        <h5><?php echo $donnees[$i]['coutService']; ?> €</h5>
+                        <p class="card-text">Description: <?php echo $donnees[$i]['detailService']; ?></p>
+                    </div>
+                </div>
 
-</div>
-<!-- /.row -->
+            </div>
+            <?php
+        }
 
+
+        ?>
+        </div>
+    </div>
 </div>
-<!-- /.container -->
+<?php
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
 <?php
 include 'footer.php';
 ?>
